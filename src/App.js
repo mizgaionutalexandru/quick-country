bb1831f8f093af52cf55f1c5941c92f6d225a846
Ionut Alexandru Mizga <mizgaionutalexandru@gmail.com>
2022-11-20 12:46:51 +0200

    added initial shown countries style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,90 @@
+import { useState } from 'react';
 import classes from './App.module.css';
 import Search from './components/Search/Search';
+import Countries from './components/Countries/Countries';
 
 function App() {
+  const [shownCountries, setShownCountries] = useState([
+    {
+      // Dummy data for styling
+      borders: ['DEU'],
+      capital: 'Copenhagen',
+      cca3: 'DNK',
+      currency: { name: 'Danish krone', symbol: 'kr' },
+      languages: ['danish'],
+      name: 'Denmark',
+      population: 5831404,
+      region: 'Europe',
+      timezones: ['UTC-04:00', 'UTC-03:00', 'UTC-01:00', 'UTC', 'UTC+01:00'],
+      flag: 'https://flagcdn.com/dk.svg',
+      coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/dk.svg',
+    },
+    {
+      borders: ['BGR', 'HUN', 'MDA', 'SRB', 'UKR'],
+      capital: 'Bucharest',
+      cca3: 'ROU',
+      currency: { name: 'Romanian leu', symbol: 'lei' },
+      languages: ['romanian'],
+      name: 'Romania',
+      population: 19286123,
+      region: 'Europe',
+      timezones: ['UTC+02:00'],
+      flag: 'https://flagcdn.com/ro.svg',
+      coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/ro.svg',
+    },
+    {
+      borders: ['BGR', 'HUN', 'MDA', 'SRB', 'UKR'],
+      capital: 'Bucharest',
+      cca3: 'ROU',
+      currency: { name: 'Romanian leu', symbol: 'lei' },
+      languages: ['romanian'],
+      name: 'Romania',
+      population: 19286123,
+      region: 'Europe',
+      timezones: ['UTC+02:00'],
+      flag: 'https://flagcdn.com/ro.svg',
+      coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/ro.svg',
+    },
+    {
+      borders: ['BGR', 'HUN', 'MDA', 'SRB', 'UKR'],
+      capital: 'Bucharest',
+      cca3: 'ROU',
+      currency: { name: 'Romanian leu', symbol: 'lei' },
+      languages: ['romanian'],
+      name: 'Romania',
+      population: 19286123,
+      region: 'Europe',
+      timezones: ['UTC+02:00'],
+      flag: 'https://flagcdn.com/ro.svg',
+      coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/ro.svg',
+    },
+    {
+      borders: ['BGR', 'HUN', 'MDA', 'SRB', 'UKR'],
+      capital: 'Bucharest',
+      cca3: 'ROU',
+      currency: { name: 'Romanian leu', symbol: 'lei' },
+      languages: ['romanian'],
+      name: 'Romania',
+      population: 19286123,
+      region: 'Europe',
+      timezones: ['UTC+02:00'],
+      flag: 'https://flagcdn.com/ro.svg',
+      coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/ro.svg',
+    },
+  ]);
+
+  const showCountry = (data) => {
+    console.log(data.flag, data.coatOfArms);
+    setShownCountries((prev) => prev.push(data));
+  };
+
   return (
     <div className={classes.App}>
       <div className={classes.content}>
-        <Search />
-        {/* countries */}
+        <Search showCountry={showCountry} />
+        <Countries shownCountries={shownCountries} />
       </div>
-      <div className={classes.map} id="map"></div>
+      {/* <div className={classes.map} id="map"></div> */}
     </div>
   );
 }
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,80 @@
+import classes from './Countries.module.css';
+import star from './../../icons/star.svg';
+import currency from './../../icons/currency.svg';
+import population from './../../icons/population.svg';
+import capital from './../../icons/capital.svg';
+import language from './../../icons/language.svg';
+import timezone from './../../icons/timezone.svg';
+import border from './../../icons/border.svg';
+
+function Countries({ shownCountries }) {
+  return (
+    <ul className={classes.list}>
+      {shownCountries.map((country) => {
+        return (
+          <article key={country.cca3}>
+            <div
+              style={{
+                backgroundImage: `url(${country.flag})`,
+              }}
+              className={classes.flag}
+              alt={`${country.name}'s flag`}
+            ></div>
+            <div className={classes.info}>
+              <header>
+                <h2>
+                  {country.name} ({country.cca3})
+                </h2>
+                <p>{country.region}</p>
+                <img src={star} alt="Save country action icon" />
+              </header>
+              <ul className={classes.details}>
+                <li>
+                  <img src={currency} alt="Country currency icon" />
+                  <p>
+                    Currency: {country.currency.name} ({country.currency.symbol}
+                    )
+                  </p>
+                </li>
+                <li>
+                  <img src={population} alt="Country population icon" />
+                  <p>Population: {country.population}</p>
+                </li>
+                <li>
+                  <img src={capital} alt="Country capital icon" />
+                  <p>Capital: {country.capital}</p>
+                </li>
+                <li>
+                  <img src={language} alt="Country language icon" />
+                  <p>
+                    Language{country.languages.length > 1 ? 's' : ''}:{' '}
+                    {country.languages.join(', ')}
+                  </p>
+                </li>
+                <li>
+                  <img src={timezone} alt="Country time zones icon" />
+                  <p>
+                    Time zone{country.timezones.length > 1 ? 's' : ''}:{' '}
+                    {country.timezones.join(', ')}
+                  </p>
+                </li>
+                <li>
+                  <img src={border} alt="Country border icon" />
+                  <p>
+                    Border{country.borders?.length > 1 ? 's' : ''}:{' '}
+                    {country.borders?.length === 0
+                      ? 'No borders'
+                      : country.borders.join(', ')}
+                  </p>
+                </li>
+              </ul>
+              <img src={country.coatOfArms} className={classes.coat} />
+            </div>
+          </article>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default Countries;
