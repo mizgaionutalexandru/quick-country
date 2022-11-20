861abbf75a8f03fa333d67d88469ea1a45780635
Ionut Alexandru Mizga <mizgaionutalexandru@gmail.com>
2022-11-20 12:45:58 +0200

    removed context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { CountryContextProvider } from './store/country-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <CountryContextProvider>
-    <App />
-  </CountryContextProvider>
-);
+root.render(<App />);
